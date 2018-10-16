a8410c029f9bc5cc6b2a06dd4e178319ed642b7a
ywang4 <wy_cloud@hotmail.com>
2018-10-16 16:31:15 -0700

    start to write s2s

--- models/Seq2seq/model.py
+++ models/Seq2seq/model.py
@@ -1,1 +1,22 @@
+'''
+    A seq2seq model with bidirectional rnn as encoder and
+'''
+
 import torch
+import torch.nn as nn
+
+class EncoderRNN(nn.Module):
+    def __init__(self,hidden_size, num_words, emb_size, num_layers=1, dropout=0):
+        super().__init__()
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        # word embeddings
+        self.embedding = nn.Embedding(num_words, emb_size)
+
+
+
+
+
+
+
+
